bf28986977ef20257d82459ac46fae6b5cd60dbe
repl.it user <replituser@example.com>
2021-03-05 02:31:48 +0000

    second commit

--- new.js
+++ new.js
@@ -3,8 +3,14 @@ function shadowadder(options) {
 
     if (options.shadow_type ===  'hard')
         options.shadow_type = '0px'
+        options.shadow_type = 'green'
+        options.shadow_type = 'red'
+        options.shadow_type = 'blue'
+        options.shadow_type = 'pink'
+        options.shadow_type = 'orange'
     else
         options.shadow_type = '15px'
+        options.shadow_type = 'grey'
 
     images.forEach(image => {
         image.style.box-shadow = `10px 10px ${options.shadow_type} 1px rgba(0, 0, 0, 0.12)`;
